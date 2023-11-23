615a21b555d4530782390c08afaad0ed134ff501
vlemonade <125125657+vlemonade@users.noreply.github.com>
2023-11-23 14:03:58 +0800

    wew

--- seniorcare/seniorcare/settings.py
+++ seniorcare/seniorcare/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-6#h1(y^pg5+v&5e^n(b5!%e1dr$ss(haab&j8e07qn24$5=796
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','900','localhost']
 
 
 # Application definition
