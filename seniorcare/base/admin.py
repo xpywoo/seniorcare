b2895f7e2530b28b1b2d415d1c800013e2650564
xpywoo <151809011+xpywoo@users.noreply.github.com>
2023-11-23 14:25:36 +0800

    Delete seniorcare directory

--- seniorcare/base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import senior_list,SMSMessage
-from django.contrib.auth.models import Group
-
-# Register your models here.
-admin.site.register(senior_list)
-admin.site.register(SMSMessage)
-#get rid of pre-registered Group
-admin.site.unregister(Group)
