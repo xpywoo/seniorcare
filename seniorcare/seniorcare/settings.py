6897d8dc15a864b136533bb7362870e58c63a555
vlemonade <125125657+vlemonade@users.noreply.github.com>
2023-11-23 13:13:03 +0800

    Update settings.py

--- seniorcare/seniorcare/settings.py
+++ seniorcare/seniorcare/settings.py
@@ -133,11 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS =[BASE_DIR / 'static']
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
